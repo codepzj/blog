d9a3ffb6183f4163123f7f0d092b741f5b6bdd74
codepzj.cn <email@codepzj.cn>
2025-01-28 20:15:26 +0800

    feat: 添加灯笼特效

--- themes/stellar/scripts/filters/index.js
+++ themes/stellar/scripts/filters/index.js
@@ -2,3 +2,8 @@
 
 hexo.extend.filter.register('after_render:html', require('./lib/img_lazyload').processSite);
 hexo.extend.filter.register('after_render:html', require('./lib/img_onerror').processSite);
+hexo.extend.injector.register(
+  "body_begin",
+  '<div id="lantern-wrapper" class="lantern-wrapper" no-select></div>',
+  "default"
+);
